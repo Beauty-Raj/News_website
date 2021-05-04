d1c93fe600a31c7f4dc09d2282d8eec8c25821f1
Beauty Raj <77147697+Beauty-Raj@users.noreply.github.com>
2021-05-04 18:10:03 +0530

    final change

--- src/components/NewsArticle.js
+++ src/components/NewsArticle.js
@@ -4,7 +4,7 @@ function NewsArticle({ data }) {
   return (
     <div className="news">
       <h1 className="news__title">{data.title}</h1>
-      <p className="news__desc">{data.description}</p>
+      <p className="news__desc">{data.description}<a href={data.url} target="_blank">Know More here</a></p>
       <span className="news__author">{data.author}</span> <br />
       <span className="news__published">{data.publishedAt}</span>
       <span className="news__source">{data.source.name}</span>
